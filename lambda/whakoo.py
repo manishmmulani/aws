34e8be746e6c03562cf413fc2dbe5f2bfc34c33c
Manish Mulani <mulanimonty@gmail.com>
2020-05-24 14:46:50 +0530

    creating lambda

---
+++ lambda/whakoo.py
@@ -0,0 +1,12 @@
+def create_menu_item(event, context):
+    print("Creating a menu item", event, context)
+    # TODO implement adding menu item to aurora
+    print("Menu item created")
+
+def get_menu_items(event, context):
+    print("Obtaining menu items", event, context)
+    # TODO implement getting menu items from aurora
+    print("Menu items obtained")
+    return {"id":1, "title":"Default Item"}
+
+
